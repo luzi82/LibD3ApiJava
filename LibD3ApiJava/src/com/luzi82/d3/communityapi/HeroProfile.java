05482799de5cc8bcc5b06689249aa9ce4e8ac021
Luzi82 <luzi82@gmail.com>
2014-07-13 17:47:47 +0800

    merge HeroProfile.Item to ItemInformation

--- LibD3ApiJava/src/com/luzi82/d3/communityapi/HeroProfile.java
+++ LibD3ApiJava/src/com/luzi82/d3/communityapi/HeroProfile.java
@@ -71,19 +71,19 @@ public class HeroProfile {
 
 	public static class Items {
 
-		public Item head;
-		public Item torso;
-		public Item feet;
-		public Item hands;
-		public Item shoulders;
-		public Item legs;
-		public Item bracers;
-		public Item mainHand;
-		public Item offHand;
-		public Item waist;
-		public Item rightFinger;
-		public Item leftFinger;
-		public Item neck;
+		public ItemInformation head;
+		public ItemInformation torso;
+		public ItemInformation feet;
+		public ItemInformation hands;
+		public ItemInformation shoulders;
+		public ItemInformation legs;
+		public ItemInformation bracers;
+		public ItemInformation mainHand;
+		public ItemInformation offHand;
+		public ItemInformation waist;
+		public ItemInformation rightFinger;
+		public ItemInformation leftFinger;
+		public ItemInformation neck;
 
 	}
 
@@ -101,12 +101,12 @@ public class HeroProfile {
 			public Items items;
 
 			public static class Items {
-				public Item special;
-				public Item mainHand;
-				public Item offHand;
-				public Item rightFinger;
-				public Item leftFinger;
-				public Item neck;
+				public ItemInformation special;
+				public ItemInformation mainHand;
+				public ItemInformation offHand;
+				public ItemInformation rightFinger;
+				public ItemInformation leftFinger;
+				public ItemInformation neck;
 			}
 
 			public Stats stats;
@@ -139,102 +139,6 @@ public class HeroProfile {
 			}
 
 		}
-	}
-
-	public static class Item {
-
-		public String id;
-		public String name;
-		public String icon;
-		public String displayColor;
-		public String tooltipParams;
-		public Item transmogItem;
-		public RandomAffixe[] randomAffixes;
-
-		public static class RandomAffixe {
-			public OneOf[] oneOf;
-
-			public static class OneOf {
-				public Attributes attributes;
-
-				public static class Attributes {
-					public Attribute[] primary;
-					public Attribute[] secondary;
-					public Attribute[] passive;
-
-					public static class Attribute {
-
-						public String text;
-						public String affixType;
-						public String color;
-
-					}
-
-				}
-
-				public AttributesRaw attributesRaw;
-
-				public static class AttributesRaw {
-					public Value<Float> Resistance_All;
-					public Value<Float> Hitpoints_Max_Percent_Bonus_Item;
-					public Value<Integer> Item_LegendaryItem_Level_Override;
-					public Value<Integer> Durability_Max;
-					@JsonProperty("Damage_Delta#Physical")
-					public Value<Integer> Damage_Delta_Physical;
-					public Value<Float> Power_Cooldown_Reduction_Percent_All;
-					public Value<Integer> Item_Legendary_Item_Base_Item;
-					public Value<Integer> Season;
-					public Value<Integer> Vitality_Item;
-					public Value<Integer> Health_Globe_Bonus_Health;
-					public Value<Integer> Durability_Max_Before_Reforge;
-					public Value<Integer> Durability_Cur;
-					@JsonProperty("Resource_Max_Bonus#Arcanum")
-					public Value<Integer> Resource_Max_Bonus_Arcanum;
-					public Value<Float> Crit_Percent_Bonus_Capped;
-					public Value<Integer> Item_Binding_Level_Override;
-					@JsonProperty("Damage_Min#Physical")
-					public Value<Integer> Damage_Min_Physical;
-					public Value<Integer> Intelligence_Item;
-					@JsonProperty("Damage_Dealt_Percent_Bonus#Cold")
-					public Value<Float> Damage_Dealt_Percent_Bonus_Cold;
-					@JsonProperty("Damage_Dealt_Percent_Bonus#Fire")
-					public Value<Float> Damage_Dealt_Percent_Bonus_Fire;
-					@JsonProperty("Damage_Dealt_Percent_Bonus#Arcane")
-					public Value<Float> Damage_Dealt_Percent_Bonus_Arcane;
-					@JsonProperty("Damage_Dealt_Percent_Bonus#Lightning")
-					public Value<Float> Damage_Dealt_Percent_Bonus_Lightning;
-					@JsonProperty("Resource_Regen_Per_Second#Arcanum")
-					public Value<Float> Resource_Regen_Per_Second_Arcanum;
-					@JsonProperty("Item_Power_Passive#ItemPassive_Unique_Ring_710_x1")
-					public Value<Float> Item_Power_Passive_ItemPassive_Unique_Ring_710_x1;
-
-					public static class Value<T> {
-						public T min;
-						public T max;
-					}
-				}
-			}
-		}
-
-		public Recipt recipe;
-
-		public static class Recipt {
-			public String id;
-			public String slug;
-			public String name;
-			public int cost;
-			public Reagent[] reagents;
-
-			public static class Reagent {
-				public int quantity;
-				public Item item;
-			}
-
-			public Item itemProduced;
-		}
-
-		public Recipt[] craftedBy;
-
 	}
 
 	public Stats stats;
--- LibD3ApiJava/src/com/luzi82/d3/communityapi/ItemInformation.java
+++ LibD3ApiJava/src/com/luzi82/d3/communityapi/ItemInformation.java
@@ -1,6 +1,6 @@
 package com.luzi82.d3.communityapi;
 
-import com.luzi82.d3.communityapi.HeroProfile.Item.Recipt;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ItemInformation {
 
@@ -9,6 +9,7 @@ public class ItemInformation {
 	public String icon;
 	public String displayColor;
 	public String tooltipParams;
+	public ItemInformation transmogItem;
 	public Integer requiredLevel;
 	public Integer itemLevel;
 	public Integer bonusAffixes;
@@ -25,10 +26,10 @@ public class ItemInformation {
 	}
 	public Type type;
 
-	public HeroProfile.Item.RandomAffixe.OneOf.Attributes attributes;
-	public HeroProfile.Item.RandomAffixe.OneOf.AttributesRaw attributesRaw;
+	public RandomAffixe.OneOf.Attributes attributes;
+	public RandomAffixe.OneOf.AttributesRaw attributesRaw;
 	
-	public HeroProfile.Item.RandomAffixe[] randomAffixes;
+	public RandomAffixe[] randomAffixes;
 	public Object[] gems;
 	public Object[] socketEffects;
 	
@@ -39,10 +40,91 @@ public class ItemInformation {
 		public Rank[] ranks;
 		public static class Rank{
 			public Integer required;
-			public HeroProfile.Item.RandomAffixe.OneOf.Attributes attributes;
-			public HeroProfile.Item.RandomAffixe.OneOf.AttributesRaw attributesRaw;
+			public RandomAffixe.OneOf.Attributes attributes;
+			public RandomAffixe.OneOf.AttributesRaw attributesRaw;
 		}
 	}
 	public Set set;
+	public Recipt recipe;
 	public Recipt[] craftedBy;
+
+	public static class RandomAffixe {
+		public OneOf[] oneOf;
+
+		public static class OneOf {
+			public Attributes attributes;
+
+			public static class Attributes {
+				public Attribute[] primary;
+				public Attribute[] secondary;
+				public Attribute[] passive;
+
+				public static class Attribute {
+
+					public String text;
+					public String affixType;
+					public String color;
+
+				}
+
+			}
+
+			public AttributesRaw attributesRaw;
+
+			public static class AttributesRaw {
+				public Value<Float> Resistance_All;
+				public Value<Float> Hitpoints_Max_Percent_Bonus_Item;
+				public Value<Integer> Item_LegendaryItem_Level_Override;
+				public Value<Integer> Durability_Max;
+				@JsonProperty("Damage_Delta#Physical")
+				public Value<Integer> Damage_Delta_Physical;
+				public Value<Float> Power_Cooldown_Reduction_Percent_All;
+				public Value<Integer> Item_Legendary_Item_Base_Item;
+				public Value<Integer> Season;
+				public Value<Integer> Vitality_Item;
+				public Value<Integer> Health_Globe_Bonus_Health;
+				public Value<Integer> Durability_Max_Before_Reforge;
+				public Value<Integer> Durability_Cur;
+				@JsonProperty("Resource_Max_Bonus#Arcanum")
+				public Value<Integer> Resource_Max_Bonus_Arcanum;
+				public Value<Float> Crit_Percent_Bonus_Capped;
+				public Value<Integer> Item_Binding_Level_Override;
+				@JsonProperty("Damage_Min#Physical")
+				public Value<Integer> Damage_Min_Physical;
+				public Value<Integer> Intelligence_Item;
+				@JsonProperty("Damage_Dealt_Percent_Bonus#Cold")
+				public Value<Float> Damage_Dealt_Percent_Bonus_Cold;
+				@JsonProperty("Damage_Dealt_Percent_Bonus#Fire")
+				public Value<Float> Damage_Dealt_Percent_Bonus_Fire;
+				@JsonProperty("Damage_Dealt_Percent_Bonus#Arcane")
+				public Value<Float> Damage_Dealt_Percent_Bonus_Arcane;
+				@JsonProperty("Damage_Dealt_Percent_Bonus#Lightning")
+				public Value<Float> Damage_Dealt_Percent_Bonus_Lightning;
+				@JsonProperty("Resource_Regen_Per_Second#Arcanum")
+				public Value<Float> Resource_Regen_Per_Second_Arcanum;
+				@JsonProperty("Item_Power_Passive#ItemPassive_Unique_Ring_710_x1")
+				public Value<Float> Item_Power_Passive_ItemPassive_Unique_Ring_710_x1;
+
+				public static class Value<T> {
+					public T min;
+					public T max;
+				}
+			}
+		}
+	}
+
+	public static class Recipt {
+		public String id;
+		public String slug;
+		public String name;
+		public int cost;
+		public Reagent[] reagents;
+
+		public static class Reagent {
+			public int quantity;
+			public ItemInformation item;
+		}
+
+		public ItemInformation itemProduced;
+	}
 }
